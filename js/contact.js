7a6f1e454a978c95d6baf1c8bd089d1d4b612c9f
Kirito-san-88 <vincentbsr88@gmail.com>
2023-12-19 16:14:00 +0100

    testing the contact form

--- js/contact.js
+++ js/contact.js
@@ -12,21 +12,25 @@ btn.addEventListener('click', () => {
   email.value = '';
   message.value = '';
 
-  if (isSent === true) {
-    alert('Votre message a bien été envoyé.');
+  if (!isSent) {
     fetch('https://kirito-san-88.github.io/portfolio/messages', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
     })
       .then((resp) => {
-        isSent = true;
-        resp.json();
+        console.log('Réponse du serveur :', resp);
+        return resp.json();
       })
-      .catch((err) =>
+      .then(() => {
+        isSent = true;
+        alert('Votre message a bien été envoyé.');
+      })
+      .catch((err) => {
         alert(
           "Il y a eu un probleme au moment de l'envoi, contactez moi par mail : vincentbsr88@gmail.com"
-        )
-      );
+        );
+        console.error("Erreur lors de l'envoi du message :", err);
+      });
   }
 });
